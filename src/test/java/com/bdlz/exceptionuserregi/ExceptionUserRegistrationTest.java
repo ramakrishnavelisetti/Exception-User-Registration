d0e7f2e22061ee752e650e8c0134af04c4a5772a
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-18 01:52:25 +0530

    Added: Test For LastName

--- src/test/java/com/bdlz/exceptionuserregi/ExceptionUserRegistrationTest.java
+++ src/test/java/com/bdlz/exceptionuserregi/ExceptionUserRegistrationTest.java
@@ -8,16 +8,27 @@ import static org.junit.Assert.*;
 public class ExceptionUserRegistrationTest {
         ExceptionUserRegistration exceptionUserRegistration = new ExceptionUserRegistration();
 
-        @Test
-        public void givenFirstName_IsProper_ReturnTrue() {
-            boolean actualResult = exceptionUserRegistration.firstName("Ramakrishna");
-            Assert.assertEquals(true, actualResult);
-        }
+    @Test
+    public void givenFirstName_IsProper_ReturnTrue() {
+        boolean actualResult = exceptionUserRegistration.firstName("Ramakrishna");
+        Assert.assertEquals(true, actualResult);
+    }
 
-        @Test
-        public void givenFirstName_IsProper_ReturnFalse() {
-            boolean actualResult = exceptionUserRegistration.firstName("RamaKrishna");
-            Assert.assertEquals(false, actualResult);
-        }
+    @Test
+    public void givenFirstName_IsProper_ReturnFalse() {
+        boolean actualResult = exceptionUserRegistration.firstName("RamaKrishna");
+        Assert.assertEquals(false, actualResult);
+    }
+    @Test
+    public void givenLastName_IsProper_ReturnTrue() {
+        boolean actualResult = exceptionUserRegistration.lastName("Velisetti");
+        Assert.assertEquals(true, actualResult);
+    }
+
+    @Test
+    public void givenLastName_IsNotProper_ReturnFalse() {
+        boolean actualResult = exceptionUserRegistration.lastName("veliseTTi");
+        Assert.assertEquals(false, actualResult);
+    }
 
 }
