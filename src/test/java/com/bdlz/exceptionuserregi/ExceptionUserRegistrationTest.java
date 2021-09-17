1ab5333080947345882781e2ddb55b169aa6f2f5
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-18 02:14:00 +0530

    Added: Test For Password Rule3

--- src/test/java/com/bdlz/exceptionuserregi/ExceptionUserRegistrationTest.java
+++ src/test/java/com/bdlz/exceptionuserregi/ExceptionUserRegistrationTest.java
@@ -74,5 +74,16 @@ public class ExceptionUserRegistrationTest {
         boolean actualResult = exceptionUserRegistration.passwordRule2("jnsdjcvcp");
         Assert.assertEquals(false, actualResult);
     }
+    @Test
+    public void givenPasswordRule3_IsProper_ReturnTrue() {
+        boolean actualResult = exceptionUserRegistration.passwordRule3("qw98hRLkdl");
+        Assert.assertEquals(true, actualResult);
+    }
+
+    @Test
+    public void givenPasswordRule3_IsNotProper_ReturnFalse() {
+        boolean actualResult = exceptionUserRegistration.passwordRule3("jnsdjcvcp");
+        Assert.assertEquals(false, actualResult);
+    }
 
 }
