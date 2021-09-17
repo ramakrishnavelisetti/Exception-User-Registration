960b085ea1a9a5772c87418ca2d1c00df84f22c1
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-18 01:39:41 +0530

    Added: Welcome Message

---
+++ src/main/java/com/bdlz/exceptionuserregi/ExceptionUserRegistration.java
@@ -0,0 +1,7 @@
+package com.bdlz.exceptionuserregi;
+
+public class ExceptionUserRegistration {
+    public static void main(String[] args) {
+        System.out.println("welcome to the exception user Registration program");
+    }
+}
